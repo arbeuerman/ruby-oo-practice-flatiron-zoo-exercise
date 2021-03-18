e60ccd5e91e531c3f176bc789ce674399a3b8949
aidyn1984 <csa.aiden@gmail.com>
2021-03-18 15:26:27 -0500

    updated zoo class

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -11,6 +11,30 @@ class Zoo
         @@all_zoos << self 
     end
     
+    def animals
+        Animal.all.select do |animal|
+            animal.zoo == self
+        end
+
+    end
+
+    def animal_species
+        animals.map do |animal|
+            animal.species 
+        end.uniq
+    end
+    def find_by_species(species)
+        animals.select do |animal|
+            animal.species == species
+        end
+    end    
+
+    def animal_nicknames
+        animals.map do |animal|
+            animal.nickname 
+        end
+    end
+
     def self.all 
         @@all_zoos
     end   
--- run.rb
+++ run.rb
@@ -8,8 +8,9 @@ require 'pry'
 chicago_lincoln_park = Zoo.new("Lincoln Park Zoo","Lincoln Park")
 san_diego_zoo = Zoo.new("San Diego Zoo", "San Diego")
 
+
 lion = Animal.new("Big Cat", 420, "Sally", chicago_lincoln_park)
 dolphin = Animal.new("Bottle Nosed Dolphin", 1000, "Bob", san_diego_zoo)
-
+lion_1 = Animal.new("Big Cat", 420, "Riley", chicago_lincoln_park)
 binding.pry
 puts "done"
